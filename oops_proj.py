0f7be861b3a0ba0b1e8bd7fbef8937e3d6f33495
souravkap04 <skapri04@gmail.com>
2026-04-24 19:41:21 +0530

    signup & signin fn added

--- oops_proj.py
+++ oops_proj.py
@@ -8,20 +8,44 @@ class chatbook:
     def menu(self):
         user_input = input(""" welcome to Chatbook !! How would you like to proceed?
                             1.Press 1 to to signup
-                           2. Press 2 to signin
-                           3. Press 3 to write a post
-                           4.Press 4 to message a friend
-                           5. Press any other key to exit """)
+                            2. Press 2 to signin
+                            3. Press 3 to write a post
+                            4.Press 4 to message a friend
+                            5. Press any other key to exit """)
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "3":
             pass
         else : 
             exit()    
+
+    def signup(self):
+        email = input("enter your email here -> ")
+        pwd = input("enter your password here -> ")
+        self.username = email
+        self.password = pwd
+        print("you have sign up successfully ")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username == '' and self.password == '':
+            print("please signup first by pressing 1 in the main menu")
+        else:
+            username = input("enter your email here -> ")
+            pwd = input("enter your password here -> ")
+            if self.username == username and self.password == pwd:
+                print("You have signed in successfully")
+                self.loggedin = True
+            else:
+                print("Please input correct credentials..") 
+                print("\n")
+                self.menu()   
+
 
 obj = chatbook()
     
